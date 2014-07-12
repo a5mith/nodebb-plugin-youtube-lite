591c6bd3bb59e3b0184b43dd8d2fdc3ffc2a081a
Aaron Smith <aaronsmith91@sky.com>
2014-07-12 04:41:32 +0100

    Update library.js

--- library.js
+++ library.js
@@ -8,7 +8,6 @@
     YoutubeLite.parse = function(postContent, callback) {
         var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
         var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
-        var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">.+<\/a>/;
 
         if (postContent.match(embedUrl)) {
             postContent = postContent.replace(embedUrl, embed);
