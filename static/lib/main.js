326df4fd59c49a4378af4444b402497cdd92c81d
a5mith <aaronsmith91@sky.com>
2014-08-06 13:54:15 +0100

    0.0.3-5
    logging

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,11 +1,26 @@
 "use strict";
 
     $(window).on('action:widgets.loaded', function() {
-        $('.js-lazyYT').lazyYT();
-    });
+        console.log('on widget load');    });
 
-    $(window).trigger('action:composer.topics.post.', function() {
-        $('.js-lazyYT').lazyYT();
-        console.log('lazyYT: fired');
-    });
+    $(window).on('action:composer.topics.post', function() {
+        console.log('on action composer topic post');    });
 
+    $(window).on('action:composer.posts.reply', function() {
+        console.log('on action composer post reply');    });
+
+    $(window).on('action:composer.posts.edit', function() {
+        console.log('on action composer post edit');    });
+
+
+    $(window).trigger('action:widgets.loaded', function() {
+        console.log('trigger action widget loaded');    });
+
+    $(window).trigger('action:composer.topics.post', function() {
+        console.log('trigger action composer topic post');    });
+
+    $(window).trigger('action:composer.posts.reply', function() {
+        console.log('trigger action composer post reply');    });
+
+    $(window).trigger('action:composer.posts.edit', function() {
+        console.log('trigger action composer post edit');    });
