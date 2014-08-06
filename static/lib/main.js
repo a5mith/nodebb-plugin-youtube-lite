524e40fe4e44b68e5c3487a6fd667e2d610152b9
a5mith <aaronsmith91@sky.com>
2014-08-06 13:16:01 +0100

    0.0.3
    Scroll Event
    Removed Logging
    Removed Deprecated Callbacked

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,11 +1,6 @@
 "use strict";
 
-    $(window).on('action:widgets.loaded', function() {
+    $(window).trigger('scroll', function() {
         $('.js-lazyYT').lazyYT();
-    });
-
-    $(window).on('action:composer.topics.post', function() {
-        $('.js-lazyYT').lazyYT();
-        console.log('lazyYT: fired');
     });
 
