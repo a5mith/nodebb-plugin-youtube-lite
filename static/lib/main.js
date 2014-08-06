817049ecdd99c33df75e321cff1edcecf682600c
a5mith <aaronsmith91@sky.com>
2014-08-06 13:57:11 +0100

    0.0.3-6

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,26 +1,5 @@
 "use strict";
 
-    $(window).on('action:widgets.loaded', function() {
-        console.log('on widget load');    });
-
-    $(window).on('action:composer.topics.post', function() {
-        console.log('on action composer topic post');    });
-
     $(window).on('action:composer.posts.reply', function() {
-        console.log('on action composer post reply');    });
-
-    $(window).on('action:composer.posts.edit', function() {
-        console.log('on action composer post edit');    });
-
-
-    $(window).trigger('action:widgets.loaded', function() {
-        console.log('trigger action widget loaded');    });
-
-    $(window).trigger('action:composer.topics.post', function() {
-        console.log('trigger action composer topic post');    });
-
-    $(window).trigger('action:composer.posts.reply', function() {
-        console.log('trigger action composer post reply');    });
-
-    $(window).trigger('action:composer.posts.edit', function() {
-        console.log('trigger action composer post edit');    });
+        $('.js-lazyYT').lazyYT();
+    });
