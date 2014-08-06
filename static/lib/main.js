1e329cdc22d844cae3e475b2c888c2f12a9e0967
a5mith <aaronsmith91@sky.com>
2014-08-06 13:59:37 +0100

    widget

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,5 +1,10 @@
 "use strict";
 
+    $(window).on('action:widgets.loaded', function() {
+        $('.js-lazyYT').lazyYT();
+    });
+
+
     $(window).on('action:composer.posts.reply', function() {
         $('.js-lazyYT').lazyYT();
     });
