0a0a0e8e99b292e7b6304a56cdfa1fc3f0229029
a5mith <aaronsmith91@sky.com>
2014-08-06 14:00:36 +0100

    logging

--- static/lib/main.js
+++ static/lib/main.js
@@ -2,9 +2,11 @@
 
     $(window).on('action:widgets.loaded', function() {
         $('.js-lazyYT').lazyYT();
+        console.log('Widgets Loaded')
     });
 
 
     $(window).on('action:composer.posts.reply', function() {
         $('.js-lazyYT').lazyYT();
+        console.log('New Post')
     });
