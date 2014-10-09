b2786e3c10a27d6c0599ae5dd9e3be44ae135c16
a5mith <aaronsmith91@sky.com>
2014-10-09 19:23:02 +0100

    test - don't update

--- library.js
+++ library.js
@@ -2,13 +2,13 @@
 	"use strict";
 
 	var YoutubeLite = {},
-		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360">https://www.youtube.com/watch?v=$1</div>';
+		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360" parameters="$2">https://www.youtube.com/watch?v=$1&$2</div>';
 
 
     YoutubeLite.parse = function(postContent, callback) {
-        var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
-        var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
-        var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">.+<\/a>/;
+        var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)([\w\-_]+){11}?&([\w\-_]+)">.+<\/a>/g;
+        var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/([\w\-_]+){11}&([\w\-_]+)">.+<\/a>/g;
+        var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+){11}&([\w\-_]+)">.+<\/a>/;
 
         if (postContent.match(embedUrl)) {
             postContent = postContent.replace(embedUrl, embed);
