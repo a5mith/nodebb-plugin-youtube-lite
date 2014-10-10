9df736c75cc6983b92af03c8b6d113af309a9fb4
a5mith <aaronsmith91@sky.com>
2014-10-10 17:40:40 +0100

    0.1.1-3

--- static/lib/lazyYT.js
+++ static/lib/lazyYT.js
@@ -1,8 +1,11 @@
 /*! LazyYT (lazy load Youtube videos plugin) - v0.3.4 - 2014-06-30
- * Usage: <div class="lazyYT" data-youtube-id="laknj093n" ratio="16:9" data-parameters="rel=0">loading...</div>
- * Copyright (c) 2014 Tyler Pearson; Licensed MIT */
-(function ($) {
+* Usage: <div class="lazyYT" data-youtube-id="laknj093n" ratio="16:9" data-parameters="rel=0">loading...</div>
+* Copyright (c) 2014 Tyler Pearson; Licensed MIT */
+
+
+;(function ($) {
     'use strict';
+
     function setUp($el) {
         var width = $el.data('width'),
             height = $el.data('height'),
@@ -11,40 +14,47 @@
             aspectRatio = ['16', '9'],
             paddingTop = 0,
             youtubeParameters = $el.data('parameters') || '';
+
         if (typeof width === 'undefined' || typeof height === 'undefined') {
-            height = 0;
-            width = '100%';
-            aspectRatio = (ratio.split(":")[1] / ratio.split(":")[0]) * 100;
-            paddingTop = aspectRatio + '%';
+          height = 0;
+          width = '100%';
+          aspectRatio = (ratio.split(":")[1] / ratio.split(":")[0]) * 100;
+          paddingTop = aspectRatio + '%';
         }
+
         $el.css({
             'position': 'relative',
             'height': height,
             'width': width,
             'padding-top': paddingTop,
-            'background': 'url(//img.youtube.com/vi/' + id + '/hqdefault.jpg) center center no-repeat',
+            'background': 'url(http://img.youtube.com/vi/' + id + '/hqdefault.jpg) center center no-repeat',
             'cursor': 'pointer',
             'background-size': 'cover'
         })
             .html('<p id="lazyYT-title-' + id + '" class="lazyYT-title"></p><div class="lazyYT-button"></div>')
             .addClass('lazyYT-image-loaded');
+
         $.getJSON('https://gdata.youtube.com/feeds/api/videos/' + id + '?v=2&alt=json', function (data) {
             $('#lazyYT-title-' + id).text(data.entry.title.$t);
         });
+
         $el.on('click', function (e) {
             e.preventDefault();
             if (!$el.hasClass('lazyYT-video-loaded') && $el.hasClass('lazyYT-image-loaded')) {
-                $el.html('<iframe class="lazytube" src="//www.youtube.com/embed/' + id + '?autoplay=1&' + youtubeParameters + '></iframe>')
+                $el.html('<iframe class="lazytube" src="//www.youtube.com/embed/' + id + '?autoplay=1"></iframe>')
                     .removeClass('lazyYT-image-loaded')
                     .removeClass('js-lazyYT')
                     .addClass('lazyYT-video-loaded');
             }
         });
+
     }
+
     $.fn.lazyYT = function () {
         return this.each(function () {
             var $el = $(this).css('cursor', 'pointer');
             setUp($el);
         });
     };
+
 }(jQuery));
