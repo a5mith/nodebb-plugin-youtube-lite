2cfcb038a4101f4045e56dc83d8d3f2c861a0012
a5mith <aaronsmith91@sky.com>
2014-10-14 23:15:37 +0100

    spacing sucks

--- library.js
+++ library.js
@@ -2,13 +2,13 @@
 	"use strict";
 
 	var YoutubeLite = {},
-		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360"><iframe class="lazytube-$1" src="//www.youtube.com/embed/$1"></iframe></div>';
+		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360"><iframe class="lazytube-$1" src="//www.youtube.com/embed/$1" border="0"></iframe></div>';
 
 
     YoutubeLite.parse = function(postContent, callback) {
-        var	regularUrl = /(?:<a (?:rel="nofollow")? href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*">[^<a]+<\/a>+)/gm;
+        var	regularUrl = /(?:<a(?: rel="nofollow")? href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*">[^<a]+<\/a>+)/gm;
         var	shortUrl = /(?:<a(?: rel="nofollow")? href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*">[^<a]+<\/a>)+/gm;
-        var	embedUrl = /(?:<a (?:rel="nofollow")? href="(?:https?:\/\/)?(?:www\.)?youtube\.com\/embed\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*">[^<a]+<\/a>+)/;
+        var	embedUrl = /(?:<a(?: rel="nofollow")? href="(?:https?:\/\/)?(?:www\.)?youtube\.com\/embed\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*">[^<a]+<\/a>+)/;
 
         if (postContent.match(embedUrl)) {
             postContent = postContent.replace(embedUrl, embed);
