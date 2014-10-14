2584e473d0fe4d4266063a144f7f4d84346fa407
a5mith <aaronsmith91@sky.com>
2014-10-14 17:53:19 +0100

    0.1.3-1

--- library.js
+++ library.js
@@ -2,13 +2,13 @@
 	"use strict";
 
 	var YoutubeLite = {},
-		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360"><iframe class="youtube-id-$1" src="//www.youtube.com/embed/$1"></iframe></div>';
+		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360">Youtube Embed...</div>';
 
 
     YoutubeLite.parse = function(postContent, callback) {
-        var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*" rel="nofollow">.+<\/a>/g;
-        var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*" rel="nofollow">.+<\/a>/g;
-        var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube\.com\/embed\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*" rel="nofollow">.+<\/a>/;
+        var	regularUrl = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*/g;
+        var	shortUrl = /(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*/g;
+        var	embedUrl = /(?:https?:\/\/)?(?:www\.)youtube\.com\/embed\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*/;
 
         if (postContent.match(embedUrl)) {
             postContent = postContent.replace(embedUrl, embed);
