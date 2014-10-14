0a2bd7d0faa1ebc79e1c4f2b1935f1af8cca441a
a5mith <aaronsmith91@sky.com>
2014-10-14 18:04:00 +0100

    needs the href

--- library.js
+++ library.js
@@ -6,9 +6,9 @@
 
 
     YoutubeLite.parse = function(postContent, callback) {
-        var	regularUrl = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*/g;
-        var	shortUrl = /(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*/g;
-        var	embedUrl = /(?:https?:\/\/)?(?:www\.)youtube\.com\/embed\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*/;
+        var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*" rel="nofollow">.+<\/a>/g;
+        var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*" rel="nofollow">.+<\/a>/g;
+        var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube\.com\/embed\/((?:[\w\-_]+){11})\??([^&]+)?(&?[\w&=]+)*" rel="nofollow"><\/a>/;
 
         if (postContent.match(embedUrl)) {
             postContent = postContent.replace(embedUrl, embed);
