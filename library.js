7891ca8d5692ad2a78d4b463e4e4274911eb645a
a5mith <aaronsmith91@sky.com>
2014-11-07 11:49:12 +0000

    Updated for NodeBB 0.6.0
    Now supports 0.6.0 of NodeBB

--- library.js
+++ library.js
@@ -3,23 +3,25 @@
 
 	var YoutubeLite = {},
 		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$1"></iframe></div>';
-	
-	var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
+
+	    var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
         var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
         var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">.+<\/a>/;
 
-    YoutubeLite.parse = function(postContent, callback) {
-        if (postContent.match(embedUrl)) {
-            postContent = postContent.replace(embedUrl, embed);
+    YoutubeLite.parse = function(data, callback) {
+        if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
         }
-        if (postContent.match(regularUrl)) {
-            postContent = postContent.replace(regularUrl, embed);
+        if (data.postData.content.match(embedUrl)) {
+            data.postData.content = data.postData.content.replace(embedUrl, embed);
         }
-        if (postContent.match(shortUrl)) {
-            postContent = postContent.replace(shortUrl, embed);
+        if (data.postData.content.match(regularUrl)) {
+            data.postData.content = data.postData.content.replace(regularUrl, embed);
         }
-
-        callback(null, postContent);
+        if (data.postData.content.match(shortUrl)) {
+            data.postData.content = data.postData.content.replace(shortUrl, embed);
+        }
+        callback(null, data);
     };
 
 	module.exports = YoutubeLite;
