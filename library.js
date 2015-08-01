aee6769d4025ad4d87bc4e2121a9dd5dc890ecc5
Aaron Smith <a5mith@users.noreply.github.com>
2015-08-01 13:23:47 +0100

    Better support

--- library.js
+++ library.js
@@ -2,24 +2,18 @@
 	"use strict";
 
 	var YoutubeLite = {},
-		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$1"></iframe></div>';
+		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$1"></iframe></div>';
 
-	var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
-        var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
-        var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">.+<\/a>/;
-
+	    var	regularUrl = /<a href="http.?:.*(youtube.com|.*be\/|.*embed\/|watch\?.*v=|(\w{11})).*?<\/a>/g;
+        
     YoutubeLite.parse = function(data, callback) {
         if (!data || !data.postData || !data.postData.content) {
             return callback(null, data);
         }
-        if (data.postData.content.match(embedUrl)) {
-            data.postData.content = data.postData.content.replace(embedUrl, embed);
-        }
         if (data.postData.content.match(regularUrl)) {
+        	
             data.postData.content = data.postData.content.replace(regularUrl, embed);
-        }
-        if (data.postData.content.match(shortUrl)) {
-            data.postData.content = data.postData.content.replace(shortUrl, embed);
+            
         }
         callback(null, data);
 
