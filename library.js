75d6d4991e934b7071351778371af250110e3864
Aaron Smith <aaronsmith91@sky.com>
2015-08-20 00:21:13 +0100

    Final Regex (I hope)

--- library.js
+++ library.js
@@ -2,9 +2,9 @@
 	"use strict";
 
 	var YoutubeLite = {},
-		embed = '<div class="js-lazyYT" data-youtube-id="$5" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$5"></iframe></div>';
+		embed = '<div class="js-lazyYT" data-youtube-id="$4" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$4"></iframe></div>';
 
-	    var	regularUrl = /<a ?(rel=\"nofollow\")? href="((https?:\/\/)(www\.)?youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|https?\:\/\/youtu\.be\/)([a-zA-Z0-9]{6,11})" ?(rel=\"nofollow\")?>.+<\/a>/g;
+	    var	regularUrl = /<a\s?(rel="nofollow")? href="((https?:\/\/www\.)?youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9]{6,11})"\s?(rel="nofollow")?>.+<\/a>/g;
         
     YoutubeLite.parse = function(data, callback) {
         if (!data || !data.postData || !data.postData.content) {
