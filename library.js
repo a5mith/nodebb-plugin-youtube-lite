df365ab5349b4d8ab893cff6880e7b6e3edce8a4
Aaron Smith <a5mith@users.noreply.github.com>
2015-08-20 09:25:13 +0000

    Regex Update

--- library.js
+++ library.js
@@ -4,7 +4,7 @@
 	var YoutubeLite = {},
 		embed = '<div class="js-lazyYT" data-youtube-id="$4" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$4"></iframe></div>';
 
-	    var	regularUrl = /<a\s?(rel="nofollow")? href="((https?:\/\/www\.)?youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})"\s?(rel="nofollow")?>.+<\/a>/g;
+	    var	regularUrl = /<a\s?(rel="nofollow")? href="((https?:\/\/www\.)?youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|(https?:\/\/)?youtu\.be\/)([a-zA-Z0-9_-]{6,11})"\s?(rel="nofollow")?>.+<\/a>/g;
         
     YoutubeLite.parse = function(data, callback) {
         if (!data || !data.postData || !data.postData.content) {
