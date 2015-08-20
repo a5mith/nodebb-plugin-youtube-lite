1ea6fbb2b9c62f7c3152dde2167afff7fffc637f
Aaron Smith <a5mith@users.noreply.github.com>
2015-08-20 09:29:13 +0000

    Update

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
 	"use strict";
 
 	var YoutubeLite = {},
-		embed = '<div class="js-lazyYT" data-youtube-id="$4" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$4"></iframe></div>';
+		embed = '<div class="js-lazyYT" data-youtube-id="$5" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$5"></iframe></div>';
 
 	    var	regularUrl = /<a\s?(rel="nofollow")? href="((https?:\/\/www\.)?youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|(https?:\/\/)?youtu\.be\/)([a-zA-Z0-9_-]{6,11})"\s?(rel="nofollow")?>.+<\/a>/g;
         
