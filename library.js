049d0752863962d3e6e8af146c3e93ce438675f8
Aaron Smith <a5mith@users.noreply.github.com>
2015-09-08 20:30:51 +0100

    formatting

--- library.js
+++ library.js
@@ -6,28 +6,27 @@
 
             var regularUrl = /<a\s?(rel="nofollow")? href="((https?:\/\/www\.)?youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|(https?:\/\/)?youtu\.be\/)([a-zA-Z0-9_-]{6,11})"\s?(rel=$
 
-    YoutubeLite.parse = function(data, callback) {
-        if (!data || !data.postData || !data.postData.content) {
-            return callback(null, data);
-        }
-        /*
-        if (data.postData.content.match(embedUrl)) {
-            data.postData.content = data.postData.content.replace(embedUrl, embed);
-        }
-        */
-        if (data.postData.content.match(regularUrl)) {
+        YoutubeLite.parse = function(data, callback) {
+                if (!data || !data.postData || !data.postData.content) {
+                return callback(null, data);
+                }
+                /* 
+                if (data.postData.content.match(embedUrl)) { 
+                data.postData.content = data.postData.content.replace(embedUrl, embed);
+                }
+                */
+                if (data.postData.content.match(regularUrl)) {
+                	
+                data.postData.content = data.postData.content.replace(regularUrl, embed);
+                    
+                }
+                /*
+                if (data.postData.content.match(shortUrl)) {
+                data.postData.content = data.postData.content.replace(shortUrl, embed);
+                }
+                */
+                callback(null, data);
+};
 
-            data.postData.content = data.postData.content.replace(regularUrl, embed);
-
-        }
-        /*
-        if (data.postData.content.match(shortUrl)) {
-            data.postData.content = data.postData.content.replace(shortUrl, embed);
-        }
-        */
-        callback(null, data);
-
-    };
-
-        module.exports = YoutubeLite;
+	module.exports = YoutubeLite;
 }(module));
