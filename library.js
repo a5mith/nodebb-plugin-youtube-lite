f99f782679fcbcbc6b9778dbafa86f4c128b6232
Aaron Smith <aaronsmith91@sky.com>
2015-11-25 18:34:33 +0100

    try again

--- library.js
+++ library.js
@@ -1,7 +1,33 @@
-(function(module) {
-	"use strict";
+"use strict";
 
-	var YoutubeLite = {},
+var controllers = require('./lib/controllers'),
+
+	plugin = {};
+
+plugin.init = function(params, callback) {
+	var router = params.router,
+		hostMiddleware = params.middleware,
+		hostControllers = params.controllers;
+		
+	// We create two routes for every view. One API call, and the actual route itself.
+	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
+
+	router.get('/admin/plugins/youtube-lite', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
+	router.get('/api/admin/plugins/youtube-lite', controllers.renderAdminPage);
+
+	callback();
+};
+
+plugin.addAdminNavigation = function(header, callback) {
+	header.plugins.push({
+		route: '/plugins/youtube-lite',
+		icon: 'fa-youtube',
+		name: 'Youtube Lite'
+	});
+
+	callback(null, header);
+};
+var YoutubeLite = {},
 	    embed = '<div class="js-lazyYT" data-youtube-id="$4" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$4"></iframe></div>';
 
 	var regularUrl = /<a.*?href="((https?:\/\/www\.)?youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|(https?:\/\/)?youtu\.be\/)([a-zA-Z0-9_-]{6,11})".*?<\/a>/g;
@@ -16,5 +42,5 @@
         callback(null, data);
 
     };
-	module.exports = YoutubeLite;
-}(module));
+
+module.exports = YoutubeLite;
